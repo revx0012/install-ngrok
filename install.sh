371f1b83c4af6ed868a1c7ced4f7db0922778cd0
RevX <127177436+revx0012@users.noreply.github.com>
2023-05-10 21:03:42 +0000

    installs ngrok.

--- install.sh
+++ install.sh
@@ -2,7 +2,13 @@
 
 # Check if running on Android and Termux
 if [[ $(uname -o) == "Android" && $(command -v termux-info) ]]; then
-  
+wget https://raw.githubusercontent.com/revx0012/install-ngrok/main/ngrok-termux.sh
+bash ngrok-termux.sh
+
+else
+
+wget https://raw.githubusercontent.com/revx0012/install-ngrok/main/ngrok-linux.sh
+bash ngrok-linux.sh
 
 fi
 
